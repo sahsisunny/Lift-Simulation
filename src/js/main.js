3de01dfc4ffb0a88bfe47647d2df238c4862482e
SahsiSunny <sahsisunny@gmail.com>
2022-12-14 05:30:43 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -33,15 +33,17 @@ liftInput.addEventListener('change', (e) => {
           liftInput.value = lift;
      }
      // Add free lift to array
-     for (let i = 0; i < lift; i++)
+     for (let i = 0; i < lift; i++) {
           freeLift.push(i);
+     }
 });
 
 // function for generate lift
 function generateLift() {
      let addedLift = '';
-     for (let i = 0; i < lift; i++)
+     for (let i = 0; i < lift; i++) {
           addedLift += `<div class="lift" lift-position="${i}" ><div class="gate"></div></div>`;
+     }
      return addedLift;
 }
 
@@ -58,7 +60,10 @@ function clickHandler(e) {
                     ${i !== floor - 1 && i !== 0 ? `<button class="btn move" id="up" btn-floor = "${i}">🔼</button><button class="btn move" id="down" btn-floor = "${i}">🔽</button>` : ``}
                </div>
                <div class="floor" floor-number = "${i + 1}" >${i === 0 ? generateLift() : ''}</div>
-               <div class="floor-level"><p>${i}</p></div>
+               <div class="floor-level"><p>
+               ${i === 0 ? `G` : ``}
+               ${i > 0 && i ? `${i}` : ``}
+               </p></div>
           </div>`;
 
      outputArea[0].innerHTML = addedFloor;                                                     // add the floor to the output area
@@ -68,11 +73,11 @@ function clickHandler(e) {
                function upLift(n, n2) {                                                        // function for move the lift up
                     if (freeLift.length > 0) {                                                 // check if there is a free lift
                          const floorPositioNumber = e.target.getAttribute('btn-floor');        // get the floor number
+
                          let lift = document.getElementsByClassName('lift');                   // get the lift
                          let rLift = lift[freeLift[0]];                                        // get the free lift
                          let liftNumber = rLift.getAttribute('lift-position');                 // get the lift number
                          let gate = document.getElementsByClassName('gate')[freeLift[0]];      // get the gate
-                         // function MoveLiftAndOthers() {                                        // function for move the lift and others
                          rLift.style.transform = `translateY(${((floorPositioNumber) * -100) - 2}%)`;    // move the lift to the floor
                          rLift.style.transition = `transform ${floorPositioNumber * n}s ease-in-out`;    // set the transition time
                          e.target.classList.add('active');                                     // add active class to the button
@@ -88,16 +93,19 @@ function clickHandler(e) {
                               freeLift.push(busyLift.shift());                            // remove the lift from the busy lift array and add the lift to the free lift array
                               freeLift.sort();                                            // sort the free lift array
                          }, `${((floorPositioNumber * n) * 1000) + 6000}`);
-                         // }
-                         // MoveLiftAndOthers();
 
                          // Print Data in the console
                          console.log(`Lift ${Number(liftNumber) + 1} is moving to floor ${floorPositioNumber} when ${e.target.id} button is clicked and the gate is opening after ${(floorPositioNumber * n2)} seconds and closing after ${(floorPositioNumber * n2) + 5} seconds.`);
                          console.log(`Free Lift: ${freeLift.length} and Busy Lift: ${busyLift.length}`);
-                    } else alert('No lift is free right now. Please wait for a while.');
+                    } else {
+                         alert('No lift is free right now. Please wait for a while.');
+                    }
                }
-               if (moveBTN[i].id === 'up') upLift(1, 1);
-               else if (moveBTN[i].id === 'down') upLift(i / 2, 2);
+               if (moveBTN[i].id === 'up') {
+                    upLift(1, 1);
+               } else if (moveBTN[i].id === 'down') {
+                    upLift(i / 2, 2);
+               }
           });
      }
 }
