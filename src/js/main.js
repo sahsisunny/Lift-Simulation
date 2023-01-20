25ccadc9bea07f5cc5342b914c1f6612bf8a89af
SahsiSunny <sahsisunny@gmail.com>
2023-01-20 17:56:23 +0530

    some changes in UI and refactor

--- src/js/main.js
+++ src/js/main.js
@@ -10,26 +10,26 @@ const liftArea = document.getElementsByClassName('building');
 
 // for Action Button
 const backBTN = document.getElementById('back');
-const resetBTN = document.getElementById('reset');
 
 // data value
 let floor = 0;
 let lift = 0;
 let freeLift = [];
 let busyLift = [];
+let liftQueue = [];
 
-floorInput.addEventListener('change', (e) => {
+
+function handleFloorInputChange(e) {
      floor = e.target.value;
-     let screenSize = window.innerWidth;
      if (floor < 0 || floor > 99) {
           alert(`You have entered ${floor} as your floor. Please enter a number between 1 and 99.`);
           floorInput.value = '5';
           floor = 5;
           floorInput.focus();
      }
-});
+}
 
-liftInput.addEventListener('change', (e) => {
+function handleLiftInputChange(e) {
      lift = Number(e.target.value);
      let screenSize = window.innerWidth;
      if (lift < 0 || lift > 15) {
@@ -45,43 +45,47 @@ liftInput.addEventListener('change', (e) => {
      }
      for (let i = 0; i < lift; i++)
           freeLift.push(i);
-});
+}
 
 // function to generate floor
 function generateBuilding() {
      let floorHTML = '';
      for (let i = floor - 1; i >= 0; i--) {
-          floorHTML +=
-               `
-          <div class="floor" id="floor${i}">
-               <div class="lift-buttons">
-               ${i === floor - 1 ? `<button id="down" class="lift-button move" data-btn-floor="${i}">⬇️</button>` : ''}
-               ${i === 0 ? `<button id="up" class="lift-button move" data-btn-floor="${i}">⬆️</button>` : ''}
-               ${i !== floor - 1 && i !== 0 ? `
-               <button id="up" class="lift-button move" data-btn-floor="${i}">⬆️</button>
-               <button id="down" class="lift-button move" data-btn-floor="${i}">⬇️</button>
-               ` : ''}
+          let liftButtons = '';
+          if (i === floor - 1) {
+               liftButtons = `<button id="down" class="lift-button move" data-btn-floor="${i}">⬇️</button>`;
+          } else if (i === 0) {
+               liftButtons = `<button id="up" class="lift-button move" data-btn-floor="${i}">⬆️</button>`;
+          } else {
+               liftButtons = `<button id="up" class="lift-button move" data-btn-floor="${i}">⬆️</button>
+               <button id="down" class="lift-button move" data-btn-floor="${i}">⬇️</button>`;
+          }
+          let liftAreaHTML = i === 0 ? generateLift(lift) : '';
+          floorHTML += `
+               <div class="floor" id="floor${i}">
+                    <div class="lift-buttons">
+                         ${liftButtons}
+                    </div>
+                    <div class="lift-area">
+                         ${liftAreaHTML}
+                    </div>
                </div>
-               <div class="lift-area">
-                    ${i === 0 ? generateLift(lift) : ''}
-               </div>
-          </div>
-          `;
+               `;
      }
      liftArea[0].innerHTML = floorHTML;
 }
 
 // function to generate lift
-function generateLift() {
-     let addedLift = '';
-     for (let i = 0; i < lift; i++) {
-          addedLift += `
-          <div class = "lift" id="lift${i}" data-current-floor="0" data-is-moving="false">
+function generateLift(numOfLifts) {
+     let liftElements = '';
+     for (let i = 0; i < numOfLifts; i++) {
+          liftElements += `
+          <div class="lift" id="lift${i}" data-current-floor="0" data-is-moving="false">
                <div class="lift-door door-left"></div>
                <div class="lift-door door-right"></div>
           </div>`;
      }
-     return addedLift;
+     return liftElements;
 }
 
 // Submit Click handler
@@ -90,102 +94,84 @@ submitBTN.addEventListener('click', (e) => {
      inputArea[0].classList.add('hide');
      outputArea[0].classList.remove('hide');
      generateBuilding();
+     addMoveEventListener();
+});
 
-     // Event Listener for Lift Button
+// Function to store lift button click
+function storeLiftButtonClick(e) {
+     e.preventDefault();
+     let [x, requestedFloorNo] = e.target.dataset.btnFloor.split("floor");
+     liftQueue.push(requestedFloorNo);
+     console.log(liftQueue);
+}
+
+
+// Function to add event listener to move button
+const addMoveEventListener = () => {
      const moveBTN = document.getElementsByClassName('move');
      for (let i = 0; i < moveBTN.length; i++) {
-          moveBTN[i].addEventListener('click', (e) => {
-               // Move Lift Function
-               function moveLift(e) {
-                    e.preventDefault();
-                    const floorDiv = e.target.parentNode.parentNode;
-                    var [x, requestedFloorNo] = floorDiv.id.split("floor");
-                    for (let i = 0; i < freeLift.length; i++) {
-                         const lift = document.getElementById(`lift${freeLift[0]}`);
-                         if (lift.dataset.isMoving === "false") {
-                              let floorCalled = Math.abs(requestedFloorNo - lift.dataset.currentFloor);
-                              let travelDuration = floorCalled * 2;
-                              let gateOpenDuration = travelDuration * 1000;
-                              let gateCloseDuration = gateOpenDuration + 2600;
-                              let resetLiftDuration = gateCloseDuration + 1000;
-                              console.log(`Lift come from ${lift.dataset.currentFloor} to ${requestedFloorNo} in ${travelDuration} seconds`);
-
-                              // Lift transform
-                              lift.style.transform = `translateY(${((requestedFloorNo) * -128)}px)`;
-                              lift.style.transition = `transform ${travelDuration}s ease-in-out`;
-                              lift.dataset.isMoving = true;
-                              e.target.classList.add('active-btn');
-
-                              // Lift Gate
-                              let lGate = document.getElementsByClassName('door-left')[freeLift[0]];
-                              let rGate = document.getElementsByClassName('door-right')[freeLift[0]];
-                              setTimeout(() => {
-                                   lGate.classList.add("animation");
-                                   rGate.classList.add("animation");
-                                   console.log("Door Open");
-                              }, `${gateOpenDuration}`);
-
-                              setTimeout(() => {
-                                   lGate.classList.remove("animation");
-                                   rGate.classList.remove("animation");
-                                   e.target.classList.remove('active-btn');
-                                   console.log("Door close");
-                              }, `${gateCloseDuration}`);
-
-                              freeLift.shift(busyLift.push(freeLift[0]));
-                              setTimeout(() => {
-                                   freeLift.push(busyLift.shift());
-                                   freeLift.sort();
-                                   lift.dataset.isMoving = false;
-                                   console.log("Lift Reset");
-                              }, `${resetLiftDuration}`);
-                              lift.setAttribute("data-current-floor", requestedFloorNo);
-                              break;
-                         }
-                    }
-               };
-               moveLift(e);
-          });
+          moveBTN[i].addEventListener('click', moveLift);
      }
-});
+}
+
+const moveLift = (e) => {
+     e.preventDefault();
+     const floorDiv = e.target.parentNode.parentNode;                                     // target floor div from move button
+     var [x, requestedFloorNo] = floorDiv.id.split("floor");                              // get floor number from floor div id
+     for (let i = 0; i < freeLift.length; i++) {                                          // loop through free lift array
+          const lift = document.getElementById(`lift${freeLift[0]}`);                     // get lift element from free lift array
+          if (lift.dataset.isMoving === "false") {                                        // check if lift is moving or not
+               let floorCalled = Math.abs(requestedFloorNo - lift.dataset.currentFloor);  // get floor difference
+               let travelDuration = floorCalled * 2;                                      // calculate travel duration
+               let gateOpenDuration = travelDuration * 1000;                              // calculate gate open duration
+               let gateCloseDuration = gateOpenDuration + 2600;                           // calculate gate close duration
+               let resetLiftDuration = gateCloseDuration + 1000;                          // calculate reset lift duration
+               console.log(`Lift come from ${lift.dataset.currentFloor} to ${requestedFloorNo} in ${travelDuration} seconds`);    // log travel duration
+               lift.style.transform = `translateY(${((requestedFloorNo) * -128)}px)`;     // move lift
+               lift.style.transition = `transform ${travelDuration}s ease-in-out`;        // set lift transition
+               lift.dataset.isMoving = true;                                              // set lift is moving to true     
+               e.target.classList.add('active-btn');                                      // add active class to move button
+
+               // Lift Gate
+               let lGate = document.getElementsByClassName('door-left')[freeLift[0]];   // get left gate element from free lift array
+               let rGate = document.getElementsByClassName('door-right')[freeLift[0]];  // get right gate element from free lift array
+               setTimeout(() => {
+                    lGate.classList.add("animation");                                    // add animation class to left gate
+                    rGate.classList.add("animation");                                    // add animation class to right gate
+                    console.log("Door Open");                                             // log door open
+               }, `${gateOpenDuration}`);
+
+               setTimeout(() => {
+                    lGate.classList.remove("animation");                                  // remove animation class from left gate
+                    rGate.classList.remove("animation");                                  // remove animation class from right gate
+                    e.target.classList.remove('active-btn');                              // remove active class from move button
+                    console.log("Door close");                                            // log door close
+               }, `${gateCloseDuration}`);
+
+               freeLift.shift(busyLift.push(freeLift[0]));                                // shift free lift to busy lift
+
+               setTimeout(() => {                                                         // reset lift  
+                    freeLift.push(busyLift.shift());                                      // shift busy lift to free lift
+                    freeLift.sort();                                                      // sort free lift array
+                    lift.dataset.isMoving = false;                                        // set lift is moving to false
+                    console.log("Lift Reset");                                            // log lift reset
+               }, `${resetLiftDuration}`);
+
+               lift.setAttribute("data-current-floor", requestedFloorNo);                 // set current floor to requested floor
+               break;
+          }
+     }
+};
+
+
 
 // Back Button Click Handler
-backBTN.addEventListener('click', (e) => {
+function showInputArea(e) {
      e.preventDefault();
-     inputArea[0].classList.remove('hide');
-     outputArea[0].classList.add('hide');
-     floorInput.focus();
+     window.location.reload();
+}
 
-});
+backBTN.addEventListener('click', showInputArea);
 
-// Reset Button Click Handler
-resetBTN.addEventListener('click', (e) => {
-
-     // remove lift door animation
-     const leftDoor = document.getElementsByClassName('door-left');
-     const rightDoor = document.getElementsByClassName('door-right');
-     for (let i = 0; i < leftDoor.length; i++) {
-          leftDoor[i].classList.remove('animation');
-          rightDoor[i].classList.remove('animation');
-     }
-     // set lift position to 0
-     for (let i = 0; i < lift; i++) {
-          document.getElementById(`lift${i}`).style.transform = `translateY(0px)`;
-          document.getElementById(`lift${i}`).setAttribute("data-current-floor", 0);
-     }
-     // set lift to free
-     for (let i = 0; i < lift; i++) {
-          freeLift.push(i);
-          busyLift = [];
-     }
-     // remove active button
-     const activeBTN = document.getElementsByClassName('active-btn');
-     for (let i = 0; i < activeBTN.length; i++) {
-          activeBTN[i].classList.remove('active-btn');
-     }
-     // remove lift animation
-     const liftAnimation = document.getElementsByClassName('lift');
-     for (let i = 0; i < liftAnimation.length; i++) {
-          liftAnimation[i].style.transition = `transform 0s ease-in-out`;
-     }
-});
+floorInput.addEventListener('change', handleFloorInputChange);
+liftInput.addEventListener('change', handleLiftInputChange);
